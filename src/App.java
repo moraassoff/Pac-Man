399434c87fc735474ce6710f49e5b47e903d35c1
moraassoff <moraassoff.scholar@gmail.com>
2025-06-28 02:59:11 -0600

    se agregan las funcionalidades finales y uso de juego totalmente corriendo

--- src/App.java
+++ src/App.java
@@ -1,26 +1,19 @@
 import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-       //determinar el tamaño de la ventana
-       int rowCount = 21;
-       int columnCount = 19;
-       int tileSize = 32;
-       int boardWidth =  columnCount * tileSize;
-       int boardHeight = rowCount * tileSize;
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
 
-
-       JFrame frame = new JFrame("Pac Man");
-       frame.setVisible(true);
-       frame.setSize(boardWidth, boardHeight);
-       frame.setLocationRelativeTo(null);
-       frame.setResizable(false);
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-
-
-
-// se crea la instancia del frame del juego
+        JFrame frame = new JFrame("Pac Man");
+        // frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         PacMan pacmanGame = new PacMan();
         frame.add(pacmanGame);
